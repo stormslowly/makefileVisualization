9f905b852a5c80cd95a836992032334ef1b12009
stormslowly <pengfei.shu@nsn.com>
2014-11-04 17:15:32 +0800

    extend array to had upush: unique push

--- test/testUniqueArray.js
+++ test/testUniqueArray.js
@@ -1,7 +1,38 @@
 'use strict';
+var expect = require('chai').expect;
 
+var UArray = require('../lib/uarray.js');
 describe('Unique Array', function() {
 
+  var ua = null;
 
-  it('it should ok');
+  beforeEach(function() {
+    ua = [];
+  });
+
+
+  it('can push', function() {
+    expect(ua.upush('first')).equal(1);
+
+  });
+
+  it('only push not-containing String item', function() {
+    expect(ua.upush('first')).equal(1);
+    expect(ua.upush('first')).equal(1);
+    expect(ua.upush('second')).equal(2);
+  });
+
+  it('only push not-containning object item', function() {
+    expect(ua.upush({
+      a: 1
+    })).to.equal(1);
+    expect(ua.upush({
+      a: 1
+    })).to.equal(1);
+    expect(ua.upush({
+      a: 2
+    })).to.equal(2);
+
+  });
+
 });
